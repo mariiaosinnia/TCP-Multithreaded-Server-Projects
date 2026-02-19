c8b928e079162bb68baedcf81b8a8ccb2e2342ab
mariiaosinnia <mosinnia@kse.org.ua>
2026-02-19 19:25:01 +0200

    feat: added method for request header parsing

---
+++ Server/RequestParser.cpp
@@ -0,0 +1,36 @@
+#include "RequestParser.h"
+#include <WinSock2.h>
+
+Request RequestParser::parseRequest(const std::vector<char>& buffer){
+	Request request;
+	size_t offset = 0;
+	request.command = static_cast<Command>(buffer[offset]);
+	offset += COMMAND_BYTES;
+
+	switch (request.command) {
+		case Command::Get:
+		case Command::Put:
+		case Command::Delete:
+		case Command::Info:
+		{
+			uint16_t file_name_length = 0;
+			memcpy(&file_name_length, buffer.data() + offset, FILENAME_LENGTH_BYTES);
+			file_name_length = ntohs(file_name_length);
+			offset += FILENAME_LENGTH_BYTES;
+
+			request.file_name = std::string(buffer.data() + offset, file_name_length);
+			offset += file_name_length;
+
+			break;
+		}
+		case Command::List: break;
+	}
+	if (request.command == Command::Put) {
+		uint32_t file_size = 0;
+		memcpy(&file_size, buffer.data() + offset, FILE_SIZE_BYTES);
+		file_size = ntohl(file_size);
+		request.file_size = file_size;
+	}
+	
+	return request;
+}
