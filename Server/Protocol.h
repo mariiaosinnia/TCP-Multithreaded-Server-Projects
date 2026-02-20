43b7ebe7f09a442cfb7d95bb55df80c86abbe537
mariiaosinnia <mosinnia@kse.org.ua>
2026-02-20 15:52:42 +0200

    feat: added file with protocol info

---
+++ Server/Protocol.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+#include <vector>
+
+const int REQUEST_HEADER_SIZE_BYTES = 4;
+const int COMMAND_BYTES = 1;
+const int FILENAME_LENGTH_BYTES = 2;
+const int FILE_SIZE_BYTES = 4;
+
+const int STATUS_BYTES = 1;
+const int PAYLOAD_LENGTH_BYTES = 4;
+
+enum class Command : uint8_t {
+	Get,
+	List,
+	Put,
+	Delete,
+	Info,
+	Unknown
+};
+
+enum class Status : uint8_t {
+	SUCCESS,
+	FILE_NOT_FOUND,
+	INVALID_REQUEST,
+	INTERNAL_ERROR
+};
+
+
+struct Request {
+	Command command;
+	std::string file_name = "";
+	uint32_t file_size = 0;
+};
