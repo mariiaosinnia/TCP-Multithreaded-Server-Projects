47b7ca4d6f873fba513fa1858a6de80ddd9c45b9
mariiaosinnia <mosinnia@kse.org.ua>
2026-02-20 13:14:54 +0200

    feat: implemented handleGet method

--- Server/CommandHandler.cpp
+++ Server/CommandHandler.cpp
@@ -13,3 +13,34 @@ void CommandHandler::sendAll(SOCKET socket, char* data, int size){
 	}
 }
 
+void CommandHandler::handleGet(SOCKET client_socket, std::string& file_name){
+	std::ifstream file(file_name, std::ios::binary);
+
+	if (!file.is_open()) {
+		uint8_t status = 1;
+		send(client_socket, reinterpret_cast<char*>(&status), STATUS_BYTES, 0);
+		return;
+	}
+	uint8_t status = 0;
+	send(client_socket, reinterpret_cast<char*>(&status), STATUS_BYTES, 0);
+
+	file.seekg(0, std::ios::end);
+	uint32_t file_size = static_cast<uint32_t>(file.tellg());
+	file.seekg(0, std::ios::beg);
+
+	uint32_t net_file_size = htonl(file_size);
+	sendAll(client_socket, reinterpret_cast<char*>(&net_file_size), sizeof(net_file_size));
+
+	const size_t CHUNK_SIZE = 1024;
+	std::vector<char> buffer(CHUNK_SIZE);
+
+	while (file) {
+		file.read(buffer.data(), CHUNK_SIZE);
+		std::streamsize bytes_read = file.gcount();
+
+		if (bytes_read > 0) {
+			sendAll(client_socket, buffer.data(), bytes_read);
+		}
+	}
+	file.close();
+}
