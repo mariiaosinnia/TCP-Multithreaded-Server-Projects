09c53f4ce007fbc1d0759b62913abbd74cf0ac9b
mariiaosinnia <mosinnia@kse.org.ua>
2026-02-21 20:59:47 +0200

    feat: implemented RequestBuilder methods

--- Client/RequestBuilder.cpp
+++ Client/RequestBuilder.cpp
@@ -39,10 +39,45 @@ std::vector<char> RequestBuilder::buildGetRequest(const std::string& file_name){
     return request_header;
 }
 
-std::vector<char> RequestBuilder::buildPutRequest(const std::string& file_name, uint32_t file_size){
+std::vector<char> RequestBuilder::buildListRequest()
+{
+    std::vector<char> request_header;
+    std::vector<char> command_serialized = serializeCommand(Command::List);
+    request_header.insert(request_header.end(), command_serialized.begin(), command_serialized.end());
 
+    return request_header;
+}
+
+std::vector<char> RequestBuilder::buildPutRequest(const std::string& file_name, uint32_t file_size){
+    std::vector<char> request_header;
+    std::vector<char> command_serialized = serializeCommand(Command::Put);
+    request_header.insert(request_header.end(), command_serialized.begin(), command_serialized.end());
+
+    std::vector<char> file_name_serialized = serializeFileName(file_name);
+    request_header.insert(request_header.end(), file_name_serialized.begin(), file_name_serialized.end());
+
+    std::vector<char> file_size_serialized = serializeFileSize(file_size);
+    request_header.insert(request_header.end(), file_size_serialized.begin(), file_size_serialized.end());
+    return request_header;
 }
 
 std::vector<char> RequestBuilder::buildDeleteRequest(const std::string& file_name){
+    std::vector<char> request_header;
+    std::vector<char> command_serialized = serializeCommand(Command::Delete);
+    request_header.insert(request_header.end(), command_serialized.begin(), command_serialized.end());
 
+    std::vector<char> file_name_serialized = serializeFileName(file_name);
+    request_header.insert(request_header.end(), file_name_serialized.begin(), file_name_serialized.end());
+    return request_header;
+}
+
+std::vector<char> RequestBuilder::buildInfoRequest(const std::string& file_name)
+{
+    std::vector<char> request_header;
+    std::vector<char> command_serialized = serializeCommand(Command::Info);
+    request_header.insert(request_header.end(), command_serialized.begin(), command_serialized.end());
+
+    std::vector<char> file_name_serialized = serializeFileName(file_name);
+    request_header.insert(request_header.end(), file_name_serialized.begin(), file_name_serialized.end());
+    return request_header;
 }
--- Client/RequestBuilder.h
+++ Client/RequestBuilder.h
@@ -10,8 +10,8 @@ private:
     std::vector<char> serializeFileSize(uint32_t file_size);
 public:
     std::vector<char> buildGetRequest(const std::string& file_name);
-    std::vector<char> buildPutRequest(const std::string& file_name, uint32_t file_size);
     std::vector<char> buildListRequest();
+    std::vector<char> buildPutRequest(const std::string& file_name, uint32_t file_size);
     std::vector<char> buildDeleteRequest(const std::string& file_name);
     std::vector<char> buildInfoRequest(const std::string& file_name);
 };
