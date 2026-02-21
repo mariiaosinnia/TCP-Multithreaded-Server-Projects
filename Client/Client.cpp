2bbea562060f8fd7efef6b4a44389cc9ccb0345a
mariiaosinnia <mosinnia@kse.org.ua>
2026-02-21 19:40:15 +0200

    feat: adapt example code to client class

---
+++ Client/Client.cpp
@@ -0,0 +1,37 @@
+#include "Client.h"
+#include <iostream>
+#include <Ws2tcpip.h>
+
+void Client::initialize() {
+	WSADATA wsa_data;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+		std::cerr << "WSAStartup failed" << std::endl;
+		return;
+	}
+}
+
+void Client::createSocket() {
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_socket == INVALID_SOCKET) {
+		std::cerr << "Error creating socket " << WSAGetLastError() << std::endl;
+		WSACleanup();
+		return;
+	}
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	InetPton(AF_INET, server_ip, &server_addr.sin_addr);
+}
+
+void Client::connectToServer(){
+	if (connect(client_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) == SOCKET_ERROR) {
+		std::cerr << "Connect failed with error: " << WSAGetLastError() << std::endl;
+		closesocket(client_socket);
+		WSACleanup();
+		return;
+	}
+}
+
+void Client::cleanUp() {
+	closesocket(client_socket);
+	WSACleanup();
+}
--- Client/Client.h
+++ Client/Client.h
@@ -1,4 +1,23 @@
 #pragma once
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
 class Client{
+private:
+	int port = 12345;
+	SOCKET client_socket;
+	PCWSTR server_ip = L"127.0.0.1";
+	sockaddr_in server_addr;
+
+	void initialize();
+	void createSocket();
+	void connectToServer();
+
+	void cleanUp();
+
+	bool recvAll(char* buffer, int size);
+	bool sendAll(char* buffer, int size);
+public:
+
 };
 
